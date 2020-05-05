3ffcacbfe2f16e4066fb8590c94cbee6f5cd3c72
Poweril <chaselynli@gmail.com>
2020-05-06 00:29:27 +0800

    config dark theme

--- app/src/main/java/com/poweril/zhihureader/SettingsActivity.java
+++ app/src/main/java/com/poweril/zhihureader/SettingsActivity.java
@@ -1,11 +1,18 @@
 package com.poweril.zhihureader;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreferenceCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -30,6 +37,37 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            // Dark theme
+            SwitchPreferenceCompat darkThemePreference = findPreference("dark_theme");
+            Configuration configuration = getResources().getConfiguration();
+            int currentNightMode = configuration.uiMode & Configuration.UI_MODE_NIGHT_MASK;
+            if (darkThemePreference != null) {
+                // Responding to system settings
+                switch (currentNightMode) {
+                    case Configuration.UI_MODE_NIGHT_NO:
+                        // Night mode is not active, we're using the light theme
+                        darkThemePreference.setChecked(false);
+                        break;
+                    case Configuration.UI_MODE_NIGHT_YES:
+                        // Night mode is active, we're using dark theme
+                        darkThemePreference.setChecked(true);
+                        break;
+                }
+                // Change app theme
+                darkThemePreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                    if (darkThemePreference.isChecked()) {
+                        // Dark theme switched on
+                        Toast.makeText(getContext(), "DARK!", Toast.LENGTH_SHORT).show();
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    } else {
+                        // Dark theme switched off
+                        Toast.makeText(getContext(), "Light!", Toast.LENGTH_SHORT).show();
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    }
+                    return true;
+                });
+            }
         }
     }
 }
